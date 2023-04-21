639e5b2784ce44c95a3d20de94415cfd59568c5d
jyeongvv <kjy812467@gmail.com>
2023-04-21 17:27:10 +0900

    go

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -1,33 +1,79 @@
 package oop;
 
 public class Car {
-    String name; // 멤버변수 name, String(문자열만 받을 수 있음)
+    String name; // 멤버변수 name <- 문자열
     int price; // 멤버변수 price <- 정수
 
-    // 입력X (매개변수 없음 / 이미 작업이 정해져있거나, ), 출력X
-    void introduce() {
-        System.out.println("난 " + name + "!");
-        // 조건문 : this.price가 1000 이상이라면 '억원'으로 표시
-        if (this.price >=10000){
-            int firstNumber = this.price / 10000;
-            int SecondNumber = this.price % 10000;
+    // 입력 X : 매개 변수가 없다. 외부에서 전달 받을 내용. 이미 작업이 다 정해져있거나
+    // 출력 X : return 없거나, return에 뭔가 안 붙었을 때
+    // 메소드에서 어떤 변수를 찾을 때
+    // 1. 매개변수
+    // 2. 멤버변수 <- 매개변수와 멤버변수 간의 이름 중복이 없다? this 안써줘도 됨
+    void introduce() { // 자기 소개
+        System.out.println("이 차의 이름은 " + name + "입니다");
+        // 조건문 : this.price가 10000 이상이라면 '억원'으로 표시
+        if (this.price >= 10000) {
+            int firstNumber = this.price / 10000; // 억원.
+            int secondNumber = this.price % 10000; // 만원.
             System.out.println(
-                    String.format("이 차의 가격은 %d억 %d 만원입니다",
-                            firstNumber, SecondNumber)
+                    String.format("이 차의 가격은 %d억 %d만원입니다",
+                            firstNumber, secondNumber)
             );
-            return; // 없을 때는 아래 실행문도 출력됨
-        }
-        System.out.println("난 " + price + "만원!");
-        // return을 하는 순간 함수는 종료
-
-
-//        void discount() {
-//            this.price = this.price * 90 / 100; // 10퍼센트 할인
-        }
-
-
-        // 입력이 없고 반환만 있는
-        // 10번 할인했을 때의 가격을 알려주는
-
+            return; // 더 이상 뭐 하지마.
+        } // else 연결하는 방법도 있음.
+        System.out.println("이 차의 가격은 " + price + "만원입니다");
+        return; // return : 함수가 종료.
     }
 
+    // 할인
+    void discount() {
+        this.price = this.price * 90 / 100; // 10% 할인.
+        // this.price에 할인한 가격을 다시 저장.
+    }
+
+    // 입력이 없고, 결과만 있는.
+    // 10번 할인했을 때의 가격을 알려주는. + 원본 가격에는 영향 X
+    int discountTenTimes() {
+        int discountNum = 10;
+        // for문을 사용해서 discountNum번의 할인을 진행
+        int discountPrice = this.price; // 원래 가격.
+        // 멤버변수를 받아오기만 했지. 다시 저장하는 일 X.
+        // int같은 원시자료형은 주소값 X. 얕은 복사 X.
+        for (int i = 0; i < discountNum; i++) {
+//            discountPrice = discountPrice * 90 / 100;
+            discountPrice *= 90;
+            discountPrice /= 100;
+        }
+        return discountPrice;
+    }
+    // 입력은 X. 출력만 있는데. 그 출력은 내부에 있는 멤버 변수를
+    // 통해 계산한 값. -> 그 멤버 변수를 다시 저장? 안했어.
+
+    // 입력은 있는데 출력은 없음.
+    void discountMany(int times) { // 몇번 할인을 할 것인가?
+//        int discountNum = 10;
+        // for문을 사용해서 discountNum번의 할인을 진행
+        int discountPrice = this.price; // 원래 가격.
+        // 멤버변수를 받아오기만 했지. 다시 저장하는 일 X.
+        // int같은 원시자료형은 주소값 X. 얕은 복사 X.
+        for (int i = 0; i < times; i++) {
+//        for (int i = 0; i < discountNum; i++) {
+//            discountPrice = discountPrice * 90 / 100;
+            discountPrice *= 90;
+            discountPrice /= 100;
+        }
+        System.out.println("discountPrice = " + discountPrice);
+        this.price = discountPrice;
+//        return discountPrice;
+    }
+
+    void discountMany2(int times) { // 몇번 할인을 할 것인가?
+        for (int i = 0; i < times; i++) discount();
+        System.out.println("discountPrice = " + this.price);
+    }
+
+    int discountMany3(int times) { // 몇번 할인을 할 것인가?
+        for (int i = 0; i < times; i++) discount();
+        return this.price;
+    }
+}
--- src/oop/Solution3.java
+++ src/oop/Solution3.java
@@ -3,17 +3,42 @@ package oop;
 // 메소드
 public class Solution3 {
     public static void main(String[] args) {
-        Car Audi = new Car();
-        Audi.name = "Audi";
-        Audi.price = 27000;
-        Audi.introduce();
+        Car ferrari = new Car();
+        ferrari.name = "페라리";
+        ferrari.price = 36000;
+        ferrari.introduce();
+        Car casper = new Car();
+        casper.name = "캐스퍼";
+        casper.price = 2000;
+        casper.introduce();
 
-//        int discountNum = 20;
-//        for (int i = 0; i < discountNum; i++) {
-//            Audi.discount();
-//        }
-////        Audi.discount();
-//        Audi.introduce();
+        int discountNum = 20;
+        // for문을 사용해서 discountNum번의 할인을 진행
+        for (int i = 0; i < discountNum; i++) {
+            ferrari.discount();
+        }
+//        ferrari.discount();
+        ferrari.introduce();
+        System.out.println("ferrari.price = " + ferrari.price);
+
+        // discountPrice라고 하는 변수를 만들고, 그 변수의 값을
+        // casper discountTenTimes()라는 메소드의 결과값으로 할당.
+        int discountPrice = casper.discountTenTimes();
+        System.out.println("discountPrice = " + discountPrice);
+        System.out.println("casper.price = " + casper.price);
+
+        System.out.println("--1--");
+        casper.price = 2000;
+        casper.discountMany(15);
+        casper.introduce();
+        System.out.println("--2--");
+        casper.price = 2000;
+        casper.discountMany2(15);
+        casper.introduce();
+        System.out.println("--3--");
+        casper.price = 2000;
+        int discountPrice3 = casper.discountMany3(15);
+        System.out.println("discountPrice3 = " + discountPrice3);
+        casper.introduce();
     }
 }
-
